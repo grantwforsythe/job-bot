84d58c538ac88673b1f82416b36a60dca61f9caa
Grant Forsythe <grantwforsythe@gmail.com>
2021-05-31 17:56:52 -0400

    Add command line interface to main.py

--- main.py
+++ main.py
@@ -6,13 +6,37 @@
 # TODO: add a docstring for the project
 # TODO: update Docker file
 # TODO: create a Jobs object
+import sys
+from urllib import response
 from utils.utils import sqlite_connect
 # from scripts.database import apply, view
 from scripts.indeed import extract, transform
 
 if __name__ == '__main__':
-    soup = extract('Data Analyst', 'Toronto')
+    print('Options: ')
+    print('1. Do you want to search Indeed?')
+    print('2. Do you apply to any postings?')
+    print('3. Do you want to view postings applied to?')
+    print('4. Quit?')
+    while True:
+        print('Response: ')
+        response = int(input())
+        if response == 1:
+            print('Job title:')
+            title = input()
+            print('Location:')
+            location = input()
 
-    df = transform(soup)
+            soup = extract(title, location)
+            df = transform(soup)
+            print(df.to_string())
+        elif response == 2:
+            pass
+        elif response == 3:
+            pass
+        elif response == 4:
+            print('Goodbye')
+            sys.exit(0)
+        else:
+            print('Enter a vaild response')
 
-    print(df.to_string())
